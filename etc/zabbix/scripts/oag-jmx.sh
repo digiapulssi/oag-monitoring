5d038f74a215be188a4eafe6d1d477787fc5fe90
Tommi Palomäki <tommi.palomaki@digia.com>
2017-10-02 11:10:20 +0300

    Do not print response time if it's -1

--- etc/zabbix/scripts/oag-jmx.sh
+++ etc/zabbix/scripts/oag-jmx.sh
@@ -169,7 +169,12 @@ case $COMMAND in
   server)
     LINE=$(grep -B 2 -A 24 'groupName = '"${ARGUMENT}"';' /tmp/oag-jmx-monitoring.cache.txt | grep "$ARGUMENT2 =")
     VALUE=$(echo "$LINE" | sed -e 's/.*= \(.*\);/\1/')
-    echo "$VALUE"
+
+    # Response time of -1 indicates that there has been no messages; thus response time cannot be measured
+    # Do not print anything in that case so that Zabbix item will have nodata
+    if ! [[ "$ARGUMENT2" =~ ^respTime.* ]] || ! [ "$VALUE" = "-1" ]; then
+      echo "$VALUE"
+    fi
   ;;
 
   ## Method block is like the following:
