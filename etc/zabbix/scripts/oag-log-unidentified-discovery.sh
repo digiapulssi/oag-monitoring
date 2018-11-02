abd7629791bebf8e730eaa404caf04676ac610dc
rabbeni <raulihiedanniemi@gmail.com>
2018-11-02 09:13:45 +0200

    New way of monitoring unconfigured connections

--- etc/zabbix/scripts/oag-log-unidentified-discovery.sh
+++ etc/zabbix/scripts/oag-log-unidentified-discovery.sh
@@ -2,31 +2,11 @@
 # Version: 1.0
 set -e
 
-# Usage: ./oag-log-unidentified-discovery.sh <path to discovery list configuration file>
-# The configuration file has the following format:
-# PATH|BACKEND|THRESHOLD_COUNT|THRESHOLD_MINUTES|ID
+CONNECTIONS=$(cat $(ls -t /opt/oracle/OAG-11.1.2.4.0/apigateway/events/processed/group-2* | head -1) | jq -r '"\(.path),\(.customMsgAtts["http.destination.host"])"' | sort -u | grep -v "^null," | grep -v ",null$")
+DISCOVERY_LIST=$(cat /opt/oracle/OAG-11.1.2.4.0/apigateway/conf/oag-discovery-list.csv | sed -e 's/^\([^|]*\)|\([^|]*\)|.*/\1,\2/g' | sort -u | grep -v PATH)
 
-CONFIG_FILE="$1"
-if [ -z "$CONFIG_FILE" ]; then
-  echo "Argument missing"
-  exit 1
-fi
-
-# The discovery returns a discovery list in zabbix LLD format with the following macros:
-#  {#REGEXP}
-# The discovery returns always just one discovered item
-# The regexp is generated so that it matches all log lines corresponding to |PATH|BACKEND| combinations that are NOT configured in configuration file
-# ie. missing configurations
-# See https://stackoverflow.com/questions/7801581/regex-for-string-not-containing-multiple-specific-words
-# REGEXP can be tested with grep -P.
-
-# Note that this requires Zabbix agent version 3.4 or later because it switches from POSIX extended syntax (which does not support
-# negative look-aheads) to PCRE (Perl Compatible Regular Expressions). See https://support.zabbix.com/browse/ZBX-3924.
-
-echo -n '{"data":[{"{#REGEXP}":"^(?!.*('
-sed -n -e 's/^\(.*\)|\(.*\)|\(.*\)|\(.*\)|\(.*\)|\(.*\)$/\1.*host":"\2/p' "$CONFIG_FILE" \
-  | sed 's/[]\.\\|$(){}?+*^]/\\\\&/g' \
-  | sed 's/"/\\&/g' \
-  | sed '$!s/$/|/' \
-  | tr -d '\n'
-echo -n ')).*legs.*\"status\":[45].*$"}]}'
+for CON in $CONNECTIONS;do
+	if ! [[ "$DISCOVERY_LIST" =~ "$CON" ]];then
+		echo $CON "<br>"
+	fi
+done
